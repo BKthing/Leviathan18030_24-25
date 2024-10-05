e5bc2ce0af01999f932960b06fd6a92702d0cbd4
celinayang13 <celinayang.atx@gmail.com>
2024-10-04 22:11:04 -0500

    Tele created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleTest.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.reefsharklibrary.data.Pose2d;
+import com.reefsharklibrary.misc.ElapsedTimer;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.OldLocalizer;
+import org.firstinspires.ftc.teamcode.subsystems.Outtake;
+import org.firstinspires.ftc.teamcode.subsystems.SubSystem;
+import org.firstinspires.ftc.teamcode.subsystems.TeleopControler;
+import org.firstinspires.ftc.teamcode.subsystems.Transfer;
+import org.firstinspires.ftc.teamcode.util.threading.MasterThread;
+import org.firstinspires.ftc.teamcode.util.threading.SubSystemData;
+
+@TeleOp
+public class TeleTest extends LinearOpMode {
+    private DcMotorEx fl, fr, bl, br;
+
+
+    Drivetrain drivetrain;
+    OldLocalizer oldLocalizer;
+    Intake intake;
+    Outtake outtake;
+    TeleopControler teleopControler;
+    Transfer transfer;
+    MasterThread masterThread;
+    Telemetry.Item loopTime;
+    Telemetry.Item temporalCount;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ElapsedTimer loopTimer = new ElapsedTimer();
+        loopTime = telemetry.addData("Loop time:", loopTimer.milliSeconds());
+        temporalCount = telemetry.addData("Temporal data", "");
+
+        masterThread = new MasterThread(hardwareMap, telemetry, gamepad1, gamepad2);
+
+
+//        teleopControler = new TeleopControler(intake, transfer, outtake, masterThread.getData());
+        oldLocalizer = new OldLocalizer(masterThread.getData());
+        drivetrain = new Drivetrain(masterThread.getData(), oldLocalizer.getLocalizer());
+        drivetrain.setDriveState(Drivetrain.DriveState.DRIVER_CONTROL);
+
+        masterThread.addSubSystems(
+                drivetrain,
+                oldLocalizer
+        );
+
+
+        waitForStart();
+        masterThread.unThreadedUpdate();
+        oldLocalizer.getLocalizer().setPoseEstimate(new Pose2d(0, 0, Math.toRadians(0)));
+
+        while ( !isStopRequested()) {
+
+            masterThread.unThreadedUpdate();
+
+            loopTime.setValue(loopTimer.milliSeconds());
+            loopTimer.reset();
+        }
+    }
+}
